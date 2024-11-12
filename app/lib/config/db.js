20dcc566ad4c566eebaa8bac312571c15799535d
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-11-12 15:09:11 +0100

    create API

--- app/lib/config/db.js
+++ app/lib/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
  export const ConnectDB = async () =>{
-    await mongoose.connect('mongodb+srv://hindjoumar4:hindjoumar4@cluster0.axjwi.mongodb.net/blog-app')
+    await mongoose.connect('mongodb+srv://hindjoumar4:hindhind@cluster0.axjwi.mongodb.net/blog-app')
     console.log("DB Connected");
 } 
