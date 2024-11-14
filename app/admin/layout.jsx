c94cd18a94d9777fb6424d41c73d8fa2236dcacd
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-11-14 17:35:20 +0100

    create add product page

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,6 +1,8 @@
 import Sidebar from "@/Components/AdminComponents/Sidebar";
 import Image from "next/image";
 import { assets } from "@/Assets/assets";
+import { ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 
 export default function Layout({children}){
@@ -8,6 +10,7 @@ export default function Layout({children}){
         <>
 
         <div className="flex">
+            <ToastContainer theme="dark"/>
             <Sidebar/>
             <div className="flex flex-col w-full">
                <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
